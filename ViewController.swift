0c9d0bd3a84f78ca26a5cf796aae9b34279c02e0
Karina Osadchaya <dyach.zero@gmail.com>
2024-11-28 10:03:27 +0300

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudyCoreData
+//
+//  Created by Karina Osadchaya on 11/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
